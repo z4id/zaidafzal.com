c37676ecde02874e76d481be7383eda6eed3ec13
Zaid Afzal <zaidafzal@protonmail.com>
2022-07-03 15:23:41 +0500

    Redirect /feedback to Google Form

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['storage.googleapis.com', 'zaidafzal.com', 'www.zaidafzal.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/feedback',
+        destination: 'https://docs.google.com/forms/d/1GvOkSRm5fIHzqTfSsbnaDvHwUZcJfJq90OZOSekVJrw',
+        permanent: false
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
